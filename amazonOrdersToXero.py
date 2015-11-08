2d32be770fd62a0995363c4beb9636e767ca90fb
David R. "Chip" Kent IV <chip@theoptimalsolution.com>
2015-11-08 13:26:24 -0600

    Merging the order and item histories to create xero data

--- amazonOrdersToXero.py
+++ amazonOrdersToXero.py
@@ -7,16 +7,54 @@
 
 import sys
 import csv
+import logging
 
-if len(sys.argv) !=2:
-	print "Usage <amznfile.csv>"
+logging.basicConfig(level=logging.INFO)
+
+def parse_key(row):
+	return (row["Order ID"], row["Carrier Name & Tracking Number"])
+
+def skip_row(row):
+	return row["Order Status"] == "Cancelled" and row["Quantity"] == "0"
+
+class OrderReport:
+	def __init__(self, fileName):
+		logging.info("Creating OrderReport(%s)",(fileName))
+		self._map = {}
+		with open(fileName) as csvfile:
+			data = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+			for row in data:
+				key = parse_key(row)
+				value = {}
+
+				for field in ["Subtotal", "Shipping Charge", "Tax Before Promotions", "Total Promotions", "Tax Charged", "Total Charged"]:
+					value[field] = (row[field].replace('$',''))
+
+				if key in self._map:
+					raise Exception("Duplicate: ", key, self._map[key], value)
+
+				self._map[key] = value
+
+	def __getitem__(self, key):
+		return self._map[key]
+
+
+
+if len(sys.argv) !=3:
+	print "Usage <amzn_item_report.csv> <amzn_order_report.csv>"
 	sys.exit(-1)
 
-fileName = sys.argv[1]
-print "Output: ", fileName
+fileNameItem = sys.argv[1]
+fileNameOrder = sys.argv[2]
+fileNameOutput = fileNameItem.replace('.csv','.NEW_TEST.csv')
+logging.warn("fileNameItem: %s", fileNameItem)
+logging.warn("fileNameOrder: %s", fileNameOrder)
+logging.warn("fileNameOutput: %s", fileNameOutput)
 
-reader = csv.DictReader(open(fileName), delimiter=',', quotechar='"')
-outfile = open(fileName.replace('.csv','.NEW.csv'),'wb')
+orderReport = OrderReport(fileNameOrder)
+
+reader = csv.DictReader(open(fileNameItem), delimiter=',', quotechar='"')
+outfile = open(fileNameOutput,'wb') #TODO rename
 writer = csv.writer(outfile, delimiter=',', quotechar='"')
 
 newHeader = [
@@ -48,12 +86,72 @@ newHeader = [
 
 writer.writerow( newHeader )
 
+#TODO restructure this section (OrderAdjustments?)
+#TODO switch to logging for debug crap
+
+orderBreakdown = {}
+
+for row in reader:
+	if skip_row(row):
+		logging.debug("**** Skipping Row ****")
+		logging.debug(row)
+		continue
+
+	key = parse_key(row)
+
+	if key not in orderBreakdown:
+		value = {"Quantity":0, "Item Total": 0.0, "Shipping soon": 0, "Item Subtotal Tax": 0.0}
+		orderBreakdown[key] = value
+
+	value = orderBreakdown[key]
+	value["Quantity"] += float(row["Quantity"])
+	value["Item Total"] += float(row["Item Total"].replace('$',''))
+	value["Item Subtotal Tax"] += float(row["Item Subtotal Tax"].replace('$',''))
+	
+	if row["Order Status"] == "Shipping soon":
+		value["Shipping soon"] += 1
+
+	
+
+for key in orderBreakdown:
+	value = orderBreakdown[key]
+	logging.debug("***************************************************")
+	logging.debug("%s %s", key, value)
+
+	try:
+		#TODO Clean up this section
+		orv = orderReport[key]
+		#print orv
+		taxAdjustment = float(orv["Tax Charged"]) - value["Item Subtotal Tax"] #TODO ???
+		totalAdjustment = float(orv["Shipping Charge"]) - float(orv["Total Promotions"]) + taxAdjustment
+		totalAdjustmentPerUnit = totalAdjustment / value["Quantity"]
+
+		value["Tax Adjustment"] = taxAdjustment
+		value["Total Adjustment"] = totalAdjustment
+		value["Total Adjustment Per Unit"] = totalAdjustmentPerUnit
+
+		v1 = float(value["Item Total"]) + totalAdjustment
+		v2 = float(orv["Total Charged"])
+		logging.debug("Consistency Check: %s %s %s %s %s %s %s %s", key, v1, v2, abs(v1-v2)<1e-5, value["Item Subtotal Tax"], orv["Tax Charged"], taxAdjustment, totalAdjustmentPerUnit )
+		
+		if abs(v1-v2) > 1e-5:
+			logging.error("**** WARN: Failed Consistency Check %s %s %s %s %s %s", key, v1, v2, v1-v2, taxAdjustment, totalAdjustment)
+
+ 
+	except KeyError as e:
+		logging.warn("**** WARN: No Order Report Entry %s **** Shipping Soon?", key)
+		value["Tax Adjustment"] = 0.0
+		value["Total Adjustment"] = 0.0
+		value["Total Adjustment Per Unit"] = 0.0
+
+
+
 for row in reader:
 	newLine = []
 
-	if row["Order Status"] == "Cancelled" and row["Quantity"] == "0":
-		print "**** Skipping Row ****"
-		print row
+	if skip_row(row):
+		logging.debug("**** Skipping Row ****")
+		logging.debug(row)
 		continue
 
 	for nh in newHeader:
@@ -72,12 +170,12 @@ for row in reader:
 		elif nh == "*UnitAmount":
 			#print "%%%", row["Item Total"].replace('$',''), row["Quantity"]
 			if row["Quantity"] == "0":
-				print row
-			value = str( float(row["Item Total"].replace('$','')) / float(row["Quantity"]) ) 
+				raise Exception("Quantity=0 row", row)
+			value = str( float(row["Item Total"].replace('$','')) / float(row["Quantity"]) + orderBreakdown[key]["Total Adjustment Per Unit"] ) 
 		elif nh == "*AccountCode":
-			value = '' #todo set account code
+			value = '' # set account code here
 		elif nh == "*TaxType":
-			value = 'Tax Exempt (0%)' #'INPUT' #todo not sure
+			value = 'Tax Exempt (0%)' 
 		else:
 			value = ''
 
@@ -86,25 +184,9 @@ for row in reader:
 
 	#print ",".join(newLine)
 	writer.writerow( newLine )
-#	"EmailAddress",
-#	"POAddressLine1",
-#	"POAddressLine2",
-#	"POAddressLine3",
-#	"POAddressLine4",
-#	"POCity",
-#	"PORegion",
-#	"POPostalCode",
-#	"POCountry",
-#	"InventoryItemCode",
-#	"Description",
-#	"TrackingName1",
-#	"TrackingOption1",
-#	"TrackingName2",
-#	"TrackingOption2",
-#	"Currency"	
 
 outfile.close()
-print "*** DONE ***"
+logging.info( "*** DONE ***" )
 
 
 
